f27cb480f413a49e1b2d46406fcd9ec2c32784e4
tharindu97 <tharindukavishna@gmail.com>
2020-03-09 09:19:27 +0530

    top_icon_completed

--- travelapp/lib/screens/home_screen.dart
+++ travelapp/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,6 +7,44 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  int _selectedIndex = 0;
+  List<IconData> _icons =[
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.walking,
+    FontAwesomeIcons.biking
+  ];
+
+  Widget _buildIcon(int index){
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+        print(_selectedIndex);
+      },
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          color:_selectedIndex == index 
+          ? Theme.of(context).accentColor
+          : Color(0xFFE7EBEE),
+          borderRadius: BorderRadius.circular(30.0)
+        ),
+        child: Icon(
+          _icons[index],
+          size: 25.0,
+          color:_selectedIndex == index 
+          ? Theme.of(context).primaryColor
+          : Color(0xFFB4C1C4),
+          ),
+      ),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +61,19 @@ class _HomeScreenState extends State<HomeScreen> {
                   fontWeight: FontWeight.bold
                 ),
               ), 
+            ),
+            SizedBox(
+             height: 20.0,               
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: _icons
+              .asMap()
+              .entries
+              .map(
+                (MapEntry map) => _buildIcon(map.key),
+              )
+              .toList()
             ),
           ],
         )),
